3a64975168331468fa8f609e826a16036dd366fc
antoine pichard <antoinexvipichard@gmail.com>
2022-11-08 13:19:55 +0100

    corection def

--- exo.py
+++ exo.py
@@ -1,38 +1,4 @@
-DEBUT
-
 print("hello world")
-
-r = 12000
-s = 1250
-e = 10
-rh = 230
-
-assertionUn = (( (365 * 3) / (24 - (16 - 8)) ) * (rh) > r ) == (e * s < r ) 
-
-assertionUnPtUn = (( (365 * 3) / (24 - (16 - 8)) ) * (rh) > r ) -> V
-1095 / 16 = 68,4375 / 
-68,4375 * 230 = 15 740,625/ 
-15 740,625 > 12000
-
-assertionUnPtDeux = (e * s < r ) -> F
-10 * 1250 = 12500/ 
-12500 < 12000
-
-(( (365 * 3) / (24 - (16 - 8)) ) * (rh) > r ) == (e * s < r ) -> F
-#assertionUn = assertionUnPtUn == assertionUnPtDeux = False
-
-assertionDeux = ((365 * 3) / (4 - (12 - 8)) * (rh) > r ) == (e * s < r ) 
-
-assertionDeuxPtUn = ((365 * 3) / (4 - (12 - 8)) * (rh) > r ) -> F
-1095 / 0 #impossible de diviser par 0
-
-
-assertionDeuxPtDeux = (e * s < r ) -> F
-10 * 1250 = 12500/ 
-12500 < 12000
-
-((365 * 3) / (4 - (12 - 8)) * (rh) > r ) == (e * s < r ) -> V
-#assertionUn = assertionUnPtUn == assertionUnPtDeux = True
 
 def retournerSixPlusTrois():
     return 6 + 3
@@ -41,32 +7,55 @@ def retournerSixPlusX(x):
     return 6 + x
     
 
-print("qui vole un " + retournerSixPlusTrois() + "vole un boeuf ")
+print("qui vole un " + retournerSixPlusX(3) + "vole un boeuf ")
 
 def add(x,y):
+    #retourner la somme de x et y
     return x + y
 
 def sub(x,y):
+    #retourner la soustraction de x par y
     return x - y
 
 def multiply(x,y):
+    #retourner la multiplication de x et y
     return x * y
 
 def divide(x,y):
-    return x / y
+    if y != 0 :
+        #retourner la division de x par y si y n'est pas egal a 0
+        return x / y
+    else:
+        #retourner un message d'erreur car on ne divise pas par 0
+        return "error"
 
 def modulo(x,y):
-    return x % y
+    if x != 0 :
+        return x % y
+    else:
+        return "error"
 
 def CalculSalaireParSeconde(SalaireHoraire, HeureParJourOuvrable,nb_JourOuvrable):
-    salaireParSeconde =  ((SalaireHoraire * HeureParJourOuvrable) * nb_JourOuvrable) / 3600
-    return salaireParSeconde
+    #Assigner a salaire annuel, le salaire par nombre d'heure travaillé par an
+    salaireAnnuel =  SalaireHoraire * HeureParJourOuvrable * nb_JourOuvrable
+    #Calculer, puis assigner a nombreParSecondeParAn , le nombre de seconde dans une année non bisextile
+    nombreDeSecondeParAn = 60 * 60 * 24 *365
+    #retourner le salaire Annuel divisé par le nombre de seconde par An
+    return salaireAnnuel / nombreDeSecondeParAn
 
 def CalculSalaireNet(SalaireBrut, coeff):
-    salaireNet = (SalaireBrut * coeff) / 100
-    return SalaireNet
+    #Calculer puis Assigner a taxe, le montant des taxes a retirer
+    taxe = SalaireBrut * (coeff / 100)
+    #retourner le salaire net final
+    return salaireNet - taxe
 
 
-FIN
-
-
+def CalculSalaire(SalaireBrut, public):
+    #si j'occupe un poste de la fonction publique
+    if public:
+        #alors je retourne le salaire brut -15% de taxe
+        return CalculSalaireNet(SalaireBrut, 15)
+    #sinon, c'est que je travaille dans le secteur privé
+    else:
+        #alors je retourne le salaire brut -23% de taxes
+        return CalculSalaireNet(SalaireBrut, 23)
