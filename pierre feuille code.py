9dabad39009df048cd46aed77521018a8b3d5aa0
antoine pichard <antoinexvipichard@gmail.com>
2022-11-14 16:46:51 +0100

    ajout erreur d'entrée pour choix

--- pierre feuille code.py
+++ pierre feuille code.py
@@ -12,8 +12,7 @@ def elementCpu(alea): #renvoie un element aleatoire pour le CPU
 
 def result(choix, elementCpu): #renvoie le resulat du round
     #resulat pour joueur avec ciseau
-    print("le CPU a joué ", elementCpu )
-    if choix == "ciseau":
+    if choix == "ciseau" or choix == "1":
         if elementCpu == "ciseau":
             return "draw"
         elif elementCpu == "feuille":
@@ -21,7 +20,7 @@ def result(choix, elementCpu): #renvoie le resulat du round
         else:
             return "lose"
     #resulat pour joueur avec feuille
-    elif choix == "feuille":
+    elif choix == "feuille" or choix == "2":
         if elementCpu == "ciseau":
             return "lose"
         elif elementCpu == "feuille":
@@ -29,13 +28,15 @@ def result(choix, elementCpu): #renvoie le resulat du round
         else:
             return "win"
     #sinon ,resulat pour joueur avec pierre
-    else:
+    elif choix == "pierre" or choix == "3":
         if elementCpu == "ciseau":
             return "win"
         elif elementCpu == "feuille":
             return "lose"
         else:
             return "draw"
+    else:
+        return ValueError
 
 
 #initialise le score
@@ -45,8 +46,11 @@ while jouer == True:
     #assigner a alea, un nombre entre 1 et 3 pour le CPU
     alea = random.randint(1,3)
     #assigner a choix, la chaine choisie par l'utilisateur
-    choix = str(input("entrez un element pour jouer(ciseau/pierre/feuille)\n"))
-    if result(choix, elementCpu(alea)) == "win":
+    choix = str(input("entrez un element pour jouer(ciseau(1)/pierre(2)/feuille(3))\n"))
+    if result(choix,elementCpu(alea)) == ValueError:
+        print("Erreur: entrez ciseau(ou 1), feuille(ou 2) ou pierre(ou 3)\n")
+        break
+    elif result(choix, elementCpu(alea)) == "win":
         #ajouter 1 au premier index du score en cas de victoire
         score[0] = score[0] + 1
         print("victoire, bravo! Ton score est de : ", score )
@@ -58,8 +62,10 @@ while jouer == True:
         #ajouter 1 au troisieme index du score en cas d'egalite
         score[2] = score[2] + 1
         print("egalité. Ton score est de " , score)
-    rejouer = str(input("souhaite tu rejouer ( envoie True(oui) ou False(non)?\n"))
-    if rejouer == "false" or rejouer == "False" or rejouer == "non":
+    rejouer = str(input("souhaite tu rejouer ( envoie True si oui)\n"))
+    if rejouer == "true" or rejouer == "True":
+        jouer = True
+    else:
         jouer = False
 #afficher le score a la fin de partie
 print("partie terminé. bravo, ton score final est de " , score)
