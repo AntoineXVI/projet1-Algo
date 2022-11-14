610961c534a14ae7d5d221c09ac8132876f32ff8
antoine pichard <antoinexvipichard@gmail.com>
2022-11-14 16:59:03 +0100

    commentaires added et rejouer corrigé

--- pierre feuille code.py
+++ pierre feuille code.py
@@ -35,6 +35,7 @@ def result(choix, elementCpu): #renvoie le resulat du round
             return "lose"
         else:
             return "draw"
+    #sinon, retourner un message d'erreur
     else:
         return ValueError
 
@@ -42,30 +43,38 @@ def result(choix, elementCpu): #renvoie le resulat du round
 #initialise le score
 score = [0, 0, 0] 
 jouer = True #initalise la variable pour jouer
+#tant que jouer est egale a vrai
 while jouer == True:
     #assigner a alea, un nombre entre 1 et 3 pour le CPU
     alea = random.randint(1,3)
     #assigner a choix, la chaine choisie par l'utilisateur
     choix = str(input("entrez un element pour jouer(ciseau(1)/pierre(2)/feuille(3))\n"))
+    #si la fonction result retourne une erreur
     if result(choix,elementCpu(alea)) == ValueError:
+        #alor afficher un message d'erreur
         print("Erreur: entrez ciseau(ou 1), feuille(ou 2) ou pierre(ou 3)\n")
+        #sortir de la boucle
         break
     elif result(choix, elementCpu(alea)) == "win":
         #ajouter 1 au premier index du score en cas de victoire
         score[0] = score[0] + 1
+        #afficher le score
         print("victoire, bravo! Ton score est de : ", score )
     elif result(choix, elementCpu(alea)) == "lose":
         #ajouter 1 au deuxieme index du score en cas de defaite
         score[1] = score[1] + 1
+        #afficher le score
         print("defaite, dommage. Ton score est de " , score)
     else:
         #ajouter 1 au troisieme index du score en cas d'egalite
         score[2] = score[2] + 1
+        #afficher le score
         print("egalité. Ton score est de " , score)
+    #assigner a rejouer le str du retour de la fonction input
     rejouer = str(input("souhaite tu rejouer ( envoie True si oui)\n"))
-    if rejouer == "true" or rejouer == "True":
-        jouer = True
-    else:
+    #si rejouer est different de "true" ou "True"
+    if rejouer != "true":
+        #changer la variable jouer a "False"
         jouer = False
 #afficher le score a la fin de partie
 print("partie terminé. bravo, ton score final est de " , score)
